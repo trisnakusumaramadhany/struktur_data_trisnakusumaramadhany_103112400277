402b22b4361cb07d4fcfbc3cfdcbd4f36b57d853
trisnakusumaramadhany <trisnadani952@gmail.com>
2025-10-01 12:58:59 +0700

    pertama

--- guided/diskkon.cpp
+++ guided/diskkon.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-
 using namespace std;
 
-int maon(){
-    double total_pembelian,diskon;
-    cout<<"berapa kamu belanja: rp. ";
-    cin>> total_pembelian;
-    if(total_pembelian >=30000)(
-        diskon = 0,1*total_pembelian;
-        cout<<"kamu dapat diskon sebesar"<<diskon<<"orang kaya lu tong";
-        
-    )
+int main() {
+    double total_pembelian, diskon;
+    cout << "Berapa kamu belanja: Rp. ";
+    cin >> total_pembelian;
+
+    if (total_pembelian >= 30000) {  // ← pakai { }
+        diskon = 0.1 * total_pembelian;
+        cout << "Kamu dapat diskon sebesar Rp. " << diskon 
+             << " orang kaya lu tong" << endl;
+    } else {
+        cout << "Belanja kamu kurang dari Rp. 30000, tidak dapat diskon." << endl;
+    }
+
+    return 0;
 }
