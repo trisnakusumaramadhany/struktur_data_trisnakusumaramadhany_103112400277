2d6f5ce251ee301556059e966f7d8a5935d24f8e
trisnakusumaramadhany <trisnadani952@gmail.com>
2025-10-01 11:47:04 +0700

    pertama

--- guided/jarijari.cpp
+++ guided/jarijari.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main(){
-    int r=20;
+    int r=30;
     int s;
 
-    s=20 + ++r;
+    s=30 + ++r;
     cout<<"nilai r saat ini" <<r<<endl;
     cout<<"nilai s saat ini" <<s<<endl;
     return 0;
